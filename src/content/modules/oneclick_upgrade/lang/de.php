4051c4140c97a15d2a4f5745294cbc7bf63b196c
Ulrich Schmidt <u.schmidt@velian.de>
2017-03-30 09:27:19 +0200

    upgrade warning notice

--- src/content/modules/oneclick_upgrade/lang/de.php
+++ src/content/modules/oneclick_upgrade/lang/de.php
@@ -3,5 +3,5 @@ define ( "TRANSLATION_ONECLICK_UPGRADE", "1-Klick Upgrade" );
 define ( "TRANSLATION_NO_NEW_VERSION_AVAILABLE", "Keine neue Version verfügbar." );
 define ( "TRANSLATION_INSTALLED_VERSION", "Installierte Version" );
 define ( "TRANSLATION_AVAILABLE_VERSION", "Verfügbare Version" );
-define ( "TRANSLATION_UPGRADE_WARNING_NOTICE", "[Sicherheitshinweise bezgl. Backup hier einfügen]" );
+define ( "TRANSLATION_UPGRADE_WARNING_NOTICE", "Ein Upgrade klappt in 99 % der Fälle ohne Probleme. Selten kann es jedoch doch zu Problemen kommen. Wir empfehlen Ihnen, VOR der Installation des Upgrades eine Sicherung der Dateien und der Datenbank Ihrer Website zu erstellen." );
 define ( "TRANSLATION_DO_CORE_UPGRADE", "Upgrade durchführen" );
--- src/content/modules/oneclick_upgrade/lang/en.php
+++ src/content/modules/oneclick_upgrade/lang/en.php
@@ -3,5 +3,5 @@ define ( "TRANSLATION_ONECLICK_UPGRADE", "1Click Upgrade" );
 define ( "TRANSLATION_NO_NEW_VERSION_AVAILABLE", "No new version available." );
 define ( "TRANSLATION_INSTALLED_VERSION", "Installed version" );
 define ( "TRANSLATION_AVAILABLE_VERSION", "available version" );
-define ( "TRANSLATION_UPGRADE_WARNING_NOTICE", "[Sicherheitshinweise bezgl. Backup hier einfügen]" );
+define ( "TRANSLATION_UPGRADE_WARNING_NOTICE", "It's recommend to do a backup of your website beforde upgrading." );
 define ( "TRANSLATION_DO_CORE_UPGRADE", "Upgrade" );
