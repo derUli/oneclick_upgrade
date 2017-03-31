dc201b4310f9c62c1d478aadfda992f5a99fae54
Ulrich Schmidt <u.schmidt@velian.de>
2017-03-31 10:29:17 +0200

    temporärer wird gelöscht bevor er neu angelegt wird

--- src/content/modules/oneclick_upgrade/controllers/CoreUpgradeController.php
+++ src/content/modules/oneclick_upgrade/controllers/CoreUpgradeController.php
@@ -36,23 +36,27 @@ class CoreUpgradeController extends Controller {
 		@set_time_limit ( 0 );
 		@ignore_user_abort ( 1 );
 		$acl = new ACL ();
-		
+
 		if (! $acl->hasPermission ( "update_system" ) or is_admin_dir () or ! $this->checkForUpgrades () or get_request_method () != "POST") {
 			return false;
 		}
-		
+
 		$jsonData = $this->getJSON ();
 		if (! $jsonData) {
 			return null;
 		}
-		
+
 		$tmpDir = Path::resolve ( "ULICMS_TMP/upgrade" );
 		$tmpArchive = Path::resolve ( "$tmpDir/upgrade.zip" );
-		
+
+		if(file_exists($tmpDir)){
+					sureRemoveDir ( $tmpDir, true );
+		}
+
 		if (! file_exists ( $tmpDir )) {
 			mkdir ( $tmpDir, 0777, true );
 		}
-		
+
 		$data = file_get_contents_wrapper ( $jsonData->file, false );
 		file_put_contents ( $tmpArchive, $data );
 		$zip = new ZipArchive ();
@@ -60,13 +64,13 @@ class CoreUpgradeController extends Controller {
 			$zip->extractTo ( $tmpDir );
 			$zip->close ();
 		}
-		
+
 		$upgradeCodeDir = Path::resolve ( "$tmpDir/ulicms" );
-		
+
 		recurse_copy ( $upgradeCodeDir, ULICMS_ROOT );
-		
-		sureRemoveDir ( $upgradeCodeDir, true );
-		
+
+		sureRemoveDir ( $tmpDir, true );
+
 		include_once Path::resolve ( "ULICMS_ROOT/update.php" );
 		return true;
 	}
